49c92bb28b0997d656973c19faa3096629555cc0
saekilek <samsrrano271@gmail.com>
2025-01-30 13:36:27 +0800

    [fraction] Added rational number in constructor

--- Fraction.py
+++ Fraction.py
@@ -1,8 +1,13 @@
 class Fraction(object):
 
-    def __init__(self, numerator=0, denominator=1):
-        #TODO
-        pass
+    def __init__(self, numerator, denominator):
+
+        if isinstance(numerator,int):
+            self.numerator = numerator
+            self.denominator = 1
+
+        #elif isinstance(numerator,str):
+        #elif isinstance(numerator,float):
 
     def gcd(a, b):
         if a == 0 or b == 0:
