1c2c6bfb2aadb2c2f26cf98a6347c26d9f5b2f62
macasalad <hannahleibaniqued12@gmail.com>
2025-01-31 12:07:14 +0800

    [fraction] Fixed error-handling for invalid inputs

--- Fraction.py
+++ Fraction.py
@@ -1,22 +1,28 @@
 class Fraction(object):
 
-    def __init__(self, numerator, denominator):
-
-        if self.denominator == 0:
+    def __init__(self, numerator=0, denominator=1):
+        if denominator == 0:
             raise ZeroDivisionError
         
-        if isinstance(numerator,int):
-            self.numerator = numerator
+        if isinstance(numerator, int):
+            if isinstance(denominator, int):
+                self.numerator = numerator
+                self.denominator = denominator
+            elif not isinstance(denominator, int):
+                print("0")
             self.denominator = 1
-        elif isinstance(numerator,str):
-            numerator = numerator.strip()
-            split = numerator.split('/')
-            numerator, denominator = int(split[0]), int(split[1])
-            self.numerator, self.denominator = numerator, denominator
-
-        if not isinstance(numerator,int) or not isinstance(denominator,int):
-            raise TypeError
-
+        
+        elif isinstance(numerator, str):
+            numerator = numerator.strip().split('/')
+            if len(numerator) == 2:
+                try:
+                    numerator, denominator = int(numerator[0]), int(numerator[1])
+                    self.numerator, self.denominator = numerator, denominator
+                except ValueError as e:
+                    print("0")
+            else:
+                print("0")
+            
     def gcd(a, b):
         if a == 0 or b == 0:
             return 0
@@ -37,3 +43,7 @@ class Fraction(object):
     def get_fraction(self):
         #TODO
         pass
+
+if __name__ == "__main__":
+    fraction = Fraction(5)
+    print(fraction.get_denominator())
