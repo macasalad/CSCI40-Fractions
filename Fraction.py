7b1720af39ef96976dbacb9dab422022c000cbac
saekilek <samsrrano271@gmail.com>
2025-02-01 19:41:40 +0800

    [fractionBranch] Added comments to constructor and gcd method

--- Fraction.py
+++ Fraction.py
@@ -4,11 +4,12 @@ class Fraction(object):
         if denominator == 0:
             raise ZeroDivisionError
         
-        if isinstance(numerator, float): 
+        # Checks the validity of the input
+        if isinstance(numerator, float):
             self.numerator, self.denominator = 0, 1
 
         elif isinstance(numerator, int):
-            if isinstance(denominator, int):
+            if isinstance(denominator, int): # Checks the validity of the denominator
                 self.numerator, self.denominator = numerator, denominator
             else:
                 self.numerator, self.denominator = 0, 1
@@ -22,21 +23,25 @@ class Fraction(object):
                         raise ZeroDivisionError
                     else:
                         self.numerator, self.denominator = numerator, denominator
-                except ValueError:
+                except ValueError: # If the inputs are not integers
                     self.numerator, self.denominator = 0, 1
             elif len(numerator) == 1:
                 try:
                     numerator = int(numerator[0])
                     self.numerator, self.denominator = numerator, 1
-                except ValueError:
+                except ValueError: # If the input is not an integer
                     self.numerator, self.denominator = 0, 1
-            else:
+            else: # If there are more than 2 inputs
                 self.numerator, self.denominator = 0, 1
 
     def gcd(a, b):
+        '''
+        Returns the greatest common divisor of 2 integers a and b
+        '''
         if a == 0 or b == 0:
             return 0
         
+        # Implements the Euclidian algorithm
         while b:
             a, b = b, a%b
         
